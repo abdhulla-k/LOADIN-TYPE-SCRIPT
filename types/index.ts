629eab0d0bc083bb3742f30090629aebcd88a2ac
abdhulla-k <abdhullak149@gmail.com>
2022-05-11 23:49:44 +0530

    learned how to add number type to a parameter in a function

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+// TypeScript's type system only helps you during development. before the code gets compiled
+
+// how to add a number type to a parameter?
+
+function add( num1: number, num2: number ) {
+    return num1 + num2;
+}
