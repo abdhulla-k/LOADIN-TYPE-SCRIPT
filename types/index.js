8e5d4e57ce4c3f4b638ef527a197fa74c9312f6e
abdhulla-k <abdhullak149@gmail.com>
2022-05-11 23:57:22 +0530

    learned number, string, boolean types

--- types/index.js
+++ types/index.js
@@ -1,1 +1,11 @@
-console.log("abdhu");
+// TypeScript's type system only helps you during development. before the code gets compiled
+// how to add a number type to a parameter?
+function add(num1, num2, printResult, Username) {
+    console.log(Username, printResult);
+    return num1 + num2;
+}
+var num1 = 2; // if you added a string insted of number, then TypeScript will show an error
+var num2 = 2;
+var printResult = true;
+var Username = "abdhu";
+add(num1, num2, printResult, Username);
--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,14 @@
 
 // how to add a number type to a parameter?
 
-function add( num1: number, num2: number ) {
+function add( num1: number, num2: number, printResult: boolean, Username: string ) {
+    console.log( Username,printResult )
     return num1 + num2;
 }
+
+let num1 = 2; // if you added a string insted of number, then TypeScript will show an error
+let num2 = 2;
+let printResult = true;
+let Username = "abdhu";
+
+add( num1, num2, printResult, Username );
